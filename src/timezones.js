366c3c3da7c1e84a8397951e21b54c78cc6deaa4
Thomas Lant <lampholder@gmail.com>
2014-04-16 00:35:39 +0100

    some css changes, apparently

--- src/timezones.js
+++ src/timezones.js
@@ -11,11 +11,49 @@ TIMEZONES = {};
         return results && decodeURIComponent(results[1].replace(/\+/g, " ")) || '';
     };
 
+    TIMEZONES.getSetupFromUrl = function() {
+        var entities = window.location.pathname.split('/');
+        entities.splice(0,1); // Remove the first, empty element
+        if (entities.length < 3) {
+            return false;
+        }
+        var primaryCityString = entities.splice(0, 1);
+        var timeInPrimaryCityString = entities.splice(0, 1);
+        var secondaryCitiesStrings = entities;
+
+
+        var primaryCityMatch = CITY.findCitiesByName(primaryCityString);
+        if (primaryCityMatch.length > 1) {
+            throw "Primary city not uniquely defined";
+            //TODO: support unique specification by tz
+        }
+        var primaryCity = primaryCityMatch[0];
+
+        var secondaryCityMatch = CITY.findCitiesByName(secondaryCitiesStrings[0]);
+        if (secondaryCityMatch.length > 1) {
+            throw "Secondary city not uniquely defined";
+            //TODO: Support unique id by tz + multiple secondary cities
+        }
+        var secondaryCity = secondaryCityMatch[0];
+
+        var dateTimeRegex = RegExp('^\d{4}-\d{2}-\d{2} \d{2}:?\d{2}$');
+        var timeRegex = RegExp('^\d{1,2}:?\d{2}$');
+
+        if (dateTimeRegex.test(timeInPrimaryCity)) {
+            //TODO: dig a usable time out of the URL
+        }
+        else if (timeRegex.test(timeInPrimaryCity)) {
+
+        }
+        else throw "Date/time incorrectly formatted";
+    };
+
     $(window).load(function () {
         $(function() {
             //$('#test').timezoneTable({'cities': [{'name': 'Seattle', 'tz': 'America/Los_Angeles'}, {'name': 'London', 'tz': 'Europe/London'}]});
             var cities = JSON.parse($.cookie('cities') || "[]");
             $('#test').timezoneTable({'cities': cities});
+            $('#test').timezoneTable('moment', moment('2001-01-01 20:15').tz('America/Los_Angeles'));
             $('#test').on('citieschanged', function (e, city, cities) {
                 $.cookie('cities', JSON.stringify(cities));
             });
